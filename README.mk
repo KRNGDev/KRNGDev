f3b2307a01a424076ec48b4ae96b97897e24e135
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-03-21 13:05:32 +0100

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,32 @@
+# Bienvenido al README de Alberto
+
+¡Hola! Mi nombre es Alberto y me gradué en Desarrollo de Aplicaciones Web con un título de grado superior. Estoy emocionado de compartir un poco sobre mí y mis habilidades contigo.
+
+## Sobre mí
+Mi enfoque principal se centra en JavaScript, Java, HTML y CSS. Además, he explorado el mundo del desarrollo con Unity, lo que me ha permitido tener una perspectiva amplia y diversa en el ámbito del desarrollo de software.
+
+Mi travesía profesional me ha llevado por el terreno del modelado 3D de joyería y la impresión 3D, lo cual ha añadido una capa creativa y técnica a mis habilidades. Ahora, estoy ansioso por canalizar esa pasión en un nuevo desafío como programador.
+
+## Habilidades
+- Desarrollo de Aplicaciones Web: JavaScript, HTML, CSS
+- Conocimientos en Node.js,Tailwind
+- Trabajo en MVC
+- Programacion en C#
+- Experiencia en Unity
+- Modelado 3D de joyería
+- Impresión 3D
+
+## Características personales
+Me caracterizo por ser una persona comprometida y proactiva, siempre en crecimiento continuo. Valoro la iniciativa y la dedicación en cualquier entorno de trabajo. Mi habilidad en comunicación y marketing complementa mis habilidades técnicas, brindándome una perspectiva única para abordar problemas y proyectos.
+
+## Objetivos
+Estoy en búsqueda de un entorno donde pueda desarrollarme plenamente en el sector de la programación. Busco un lugar que valore la iniciativa y la dedicación, donde pueda aprender y aportar en un entorno colaborativo.
+
+Estoy emocionado por la oportunidad de conectarme contigo y explorar cómo puedo contribuir y crecer en el mundo del desarrollo. Espero poder ser parte de un equipo apasionado que comparta mi entusiasmo por la tecnología y la innovación.
+
+¡Gracias por tomarte el tiempo para conocerme! Estoy ansioso por lo que el futuro pueda deparar y espero poder contribuir de manera significativa a cualquier proyecto en el que esté involucrado.
+
+¡Hagamos cosas increíbles juntos! 🚀
+
+---
+*Este README fue creado por Alberto, un entusiasta del desarrollo de software en busca de nuevas oportunidades y desafíos. Si estás interesado en colaborar o conocer más sobre mí, ¡no dudes en contactarme!*
